94ecfe073f63ef3a032fcd37e5e48289ffdb60a0
kerim1 <kerim.meijer@di.huc.knaw.nl>
2023-11-10 09:49:19 +0100

    Fix header and footer

--- src/frontend/src/pageElements/footer.jsx
+++ src/frontend/src/pageElements/footer.jsx
@@ -4,24 +4,36 @@ export default function Footer() {
             <div className="py-12 px-6 w-full max-w-[1700px] mx-auto flex flex-col md:flex-row justify-center gap-12">
                 <div className="w-full border-t border-diploblue-600 pt-4">
                     <nav className="flex flex-col">
-                        <a href="/wordpress.html" className="text-white">
+                        <a href="https://diplomatieke-getuigenissen.huygens.knaw.nl/"
+                           className="text-white hover:underline">
                             Home
                         </a>
 
-                        <a href="/zoeken" className="text-white">
-                            Getuigenissen
+                        <a href="/zoeken" className="text-white hover:underline">
+                            Zoeken in collectie
                         </a>
 
-                        <a href="/wordpress2.html" className="text-white">
+                        <a href="/diplomaten" className="text-white hover:underline">
+                            Interviews
+                        </a>
+
+                        <a href="https://diplomatieke-getuigenissen.huygens.knaw.nl/index.php/over-dit-project/"
+                           className="text-white hover:underline">
                             Over dit project
+                        </a>
+
+                        <a href="https://diplomatieke-getuigenissen.huygens.knaw.nl/index.php/colofon/"
+                           className="text-white hover:underline">
+                            Colofon
                         </a>
                     </nav>
                 </div>
 
                 <div className="w-full border-t border-diploblue-600 pt-4">
                     <p className="max-w-sm">
-                        <strong>Diplomatieke Getuigenissen</strong> is een project van het <a
-                        className="text-white underline" href="https://huygens.knaw.nl/">Huygens Instituut</a>
+                        <strong>Diplomatiek Getuigenissen</strong> is een project van het <a
+                        className="text-white underline" target="_blank" href="https://huygens.knaw.nl/">
+                        Huygens Instituut</a>
                     </p>
                 </div>
 
--- src/frontend/src/pageElements/header.jsx
+++ src/frontend/src/pageElements/header.jsx
@@ -1,41 +1,46 @@
 import logo from '../assets/img/logo-huygens-instituut.png';
-import {SearchIcon} from "../misc/icons.jsx";
 
 export default function Header() {
     return (
         <header className="border-b border-diploblue-600 text-diploblue-700">
             <div className="px-6 w-full max-w-[1700px] mx-auto flex flex-col lg:flex-row items-center justify-between">
-                <div className="mt-4 mb-2 lg:mt-0 flex flex-row items-center justify-start">
-                    <a href="/index.html" className="text-diploblue-800">
-                        <span className="font-semibold">Diplomatieke Getuigenissen</span>
-                    </a>
+                <div className="mt-4 md:mt-0 flex flex-row items-center justify-start">
+                    <span className="font-semibold">
+                        <a href="https://diplomatieke-getuigenissen.huygens.knaw.nl" className="text-diploblue-700">
+                            Diplomatieke Getuigenissen
+                        </a>
+                    </span>
                 </div>
 
                 <div className="flex flex-col md:flex-row  gap-8">
-                    <nav className="flex flex-row flex-wrap justify-center items-center text-sm md:text-base">
-                        <a href="/wordpress.html"
-                           className="text-diploblue-800 py-1 lg:py-6 px-2 border-b-4 border-white hover:border-diploblue-700 hover:bg-diploblue-50">
+                    <nav className="flex flex-row items-center text-sm md:text-base">
+                        <a href="https://diplomatieke-getuigenissen.huygens.knaw.nl/" aria-current="page"
+                           className="text-diploblue-800 py-2 md:py-6 px-2 border-b-4 border-white hover:border-diploblue-700 hover:bg-diploblue-50">
                             Home
                         </a>
 
                         <a href="/zoeken"
-                           className="text-diploblue-800 py-1 lg:py-6 px-2 border-b-4 border-white hover:border-diploblue-700 hover:bg-diploblue-50 flex items-center gap-1">
-                            <SearchIcon className="w-4 h-4 fill-diploblue-800"/>
+                           className="text-diploblue-800 py-2 md:py-6 px-2 border-b-4 border-white hover:border-diploblue-700 hover:bg-diploblue-50">
                             Zoeken in collectie
                         </a>
 
                         <a href="/diplomaten"
-                           className="text-diploblue-800 py-1 lg:py-6 px-2 border-b-4 border-white hover:border-diploblue-700 hover:bg-diploblue-50">
+                           className="text-diploblue-800 py-2 md:py-6 px-2 border-b-4 border-white hover:border-diploblue-700 hover:bg-diploblue-50">
                             Interviews
                         </a>
 
-                        <a href="/wordpress2.html"
-                           className="text-diploblue-800 py-1 lg:py-6 px-2 border-b-4 border-white hover:border-diploblue-700 hover:bg-diploblue-50">
+                        <a href="https://diplomatieke-getuigenissen.huygens.knaw.nl/index.php/over-dit-project/"
+                           className="text-diploblue-800 py-2 md:py-6 px-2 border-b-4 border-white hover:border-diploblue-700 hover:bg-diploblue-50">
                             Over dit project
+                        </a>
+
+                        <a href="https://diplomatieke-getuigenissen.huygens.knaw.nl/index.php/colofon/"
+                           className="text-diploblue-800 py-2 md:py-6 px-2 border-b-4 border-white hover:border-diploblue-700 hover:bg-diploblue-50">
+                            Colofon
                         </a>
                     </nav>
 
-                    <div className="flex-row items-center hidden lg:flex">
+                    <div className="flex-row items-center hidden md:flex">
                         <div>
                             <img src={logo} alt="" className="h-6 sm:h-8 md:-mt-2"/>
                         </div>
